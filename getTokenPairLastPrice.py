5397d239c40b1e9f39aa89ea0331ee03758f9172
Alina <alina.d@superhow.org>
2022-03-22 15:41:03 +0200

    feat: customizable token pair

--- getTokenPairLastPrice.py
+++ getTokenPairLastPrice.py
@@ -1,13 +1,14 @@
 from bitmart.api_contract import APIContract
 from tests import data as data
 import Aphid
+import sys
 import time
 
 # contract api
 contractAPI = APIContract(data.api_key, data.secret_key, data.memo, data.url)
 
 def checkAuthenticationErrors():
-    assert (contractAPI.get_ticker(contract_symbol='ETHUSDT')[0]['code'] == 1000), contractAPI.get_ticker(contract_symbol='ETHUSDT')[0]['message']
+    assert (contractAPI.get_ticker(contract_symbol=str(sys.argv[1]))[0]['code'] == 1000), contractAPI.get_ticker(contract_symbol=str(sys.argv[1]))[0]['message']
 
 def print_user_data():
     APIdata = get_last_price()
@@ -19,7 +20,7 @@ def print_user_data():
 def get_last_price():
     """Test GET https://api-cloud.bitmart.com/contract/v1/tickers"""
     checkAuthenticationErrors()
-    return(contractAPI.get_ticker(contract_symbol='ETHUSDT')[0])
+    return(contractAPI.get_ticker(contract_symbol=str(sys.argv[1]))[0])
 
 while True:
     print_user_data()
